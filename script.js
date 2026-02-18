43170ad2e6a7ce87b72ed1c584a94cb18d672010
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2026-02-18 14:19:27 -0500

    set up a 6x7 game board

---
+++ script.js
@@ -0,0 +1,32 @@
+var playerRed = "R";
+var playerYellow = "Y";
+var currPlayer = playerRed;
+
+var gameOver = false;
+var board;
+
+var rows = 6;
+var columns = 7;
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    board = [];
+
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            // JS
+            row.push(' ');
+
+            //HTML
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            document.getElementById("board").append(tile);
+        }
+        board.push(row);
+    }
+}
